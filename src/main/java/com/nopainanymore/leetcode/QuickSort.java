6151739198316474abb3fa4ba9e0fd6b71a22fcd
Lee875083146 <vbof53@163.com>
2019-07-06 21:53:10 +0800

    quickSort and ThreeSum

---
+++ src/main/java/com/nopainanymore/leetcode/QuickSort.java
@@ -0,0 +1,51 @@
+package com.nopainanymore.leetcode;
+
+/**
+ * leet-code: QuickSort
+ *
+ * @author NoPainAnymore
+ * @date 2019-07-06 19:35
+ */
+public class QuickSort {
+
+
+    public static void main(String[] args) {
+        int[] arr = {4, 1, 5, 6, 7, 2, 8, 12, 46};
+        quickSort(arr, 0, arr.length - 1);
+        for (int value : arr) {
+            System.out.println(value);
+        }
+    }
+
+    public static void quickSort(int[] arr, int left, int right) {
+        if (arr == null || left > right || arr.length <= 1) {
+            return;
+        }
+        int mid = partition(arr, left, right);
+        quickSort(arr, left, mid - 1);
+        quickSort(arr, mid + 1, right);
+    }
+
+    public static int partition(int[] arr, int left, int right) {
+        int temp = arr[left];
+        while (left < right) {
+            while (left < right && temp <= arr[right]) {
+                right--;
+            }
+            if (left < right) {
+                arr[left] = arr[right];
+                left++;
+            }
+            while (left < right && temp >= arr[left]) {
+                left++;
+            }
+            if (left < right) {
+                arr[right] = arr[left];
+                right--;
+            }
+        }
+        arr[left] = temp;
+        return left;
+    }
+
+}
---
+++ src/main/java/com/nopainanymore/leetcode/ThreeSum.java
@@ -0,0 +1,68 @@
+package com.nopainanymore.leetcode;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * leet-code: ThreeSum
+ *
+ * @author NoPainAnymore
+ * @date 2019-07-06 19:31
+ * LeetCode 15
+ */
+@Slf4j
+public class ThreeSum {
+
+
+    public static List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> result = new ArrayList<>();
+        int cur, left, right;
+        for (int i = 0; i < nums.length - 2; i++) {
+            cur = nums[i];
+            if (cur > 0) {
+                break;
+            }
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            left = i + 1;
+            right = nums.length - 1;
+            while (left < right) {
+                if (0 < cur + nums[left] + nums[right]) {
+                    right--;
+                } else if (0 > cur + nums[left] + nums[right]) {
+                    left++;
+                } else {
+                    log.info("ThreeSum- threeSum- {},{},{}", cur, nums[left], nums[right]);
+                    List<Integer> solution = new ArrayList<>();
+                    solution.add(nums[i]);
+                    solution.add(nums[left]);
+                    solution.add(nums[right]);
+                    result.add(solution);
+                    while (left < right && nums[left] == nums[left + 1]) {
+                        left++;
+                    }
+                    while (left < right && nums[right] == nums[right - 1]) {
+                        right--;
+                    }
+                    left++;
+                    right--;
+                }
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] a = {-1, 0, 1, 2, -1, -4};
+        int[] b = {0, 0, 0, 0};
+        threeSum(a);
+        threeSum(b);
+    }
+
+
+}
